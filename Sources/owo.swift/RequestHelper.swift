173dec966ae31fddd56a7bf17f746c4f088eb264
Spotlight <spotlight@joscomputing.space>
2021-06-15 16:39:55 -0700

    Extend response for success value

--- Sources/owo.swift/RequestHelper.swift
+++ Sources/owo.swift/RequestHelper.swift
@@ -8,37 +8,34 @@
 import Alamofire
 import Foundation
 
+public extension DataResponse {
+    func getSuccess<Value: Decodable>() throws -> Value {
+        var result: Value
+        
+        switch self.result {
+        case let .success(parsedValue):
+            result = parsedValue as! Value
+        case let .failure(error):
+            throw error
+        }
+        
+        return result
+    }
+}
+
 public extension DataRequest {
     func handle<Value: Decodable>(type givenType: Value.Type = Value.self) async throws -> Value {
         // Check success first.
         let initialResponse = await decode(Base.self).response
-        var successBody: Base
-
-        switch initialResponse.result {
-        case let .success(parsedValue):
-            successBody = parsedValue
-        case let .failure(error):
-            throw error
-        }
+        let successBody: Base = try initialResponse.getSuccess()
 
         if successBody.success == false {
-            let errorResponse = await decode(ErrorInfo.self).response
-
-            switch errorResponse.result {
-            case let .success(parsedValue):
-                throw APIError.serviceError(errorCode: parsedValue.errorCode, reason: parsedValue.description)
-            case let .failure(error):
-                throw error
-            }
+            let error = await decode(ErrorInfo.self).response
+            let errorResponse: ErrorInfo = try error.getSuccess()
+            throw APIError.serviceError(errorCode: errorResponse.errorCode, reason: errorResponse.description)
         } else {
-            let initialResponse = await decode(givenType).response
-
-            switch initialResponse.result {
-            case let .success(parsedValue):
-                return parsedValue
-            case let .failure(error):
-                throw error
-            }
+            // We have no error, so we can return what the user specified.
+            return try await decode(givenType).response.getSuccess()
         }
     }
 }
